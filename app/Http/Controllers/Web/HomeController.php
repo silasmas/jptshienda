d4d02b8e512d48dcd8de2d4ba61cc2ae6eeb6f6f
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-08-24 11:43:14 +0100

    n

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -1,11 +1,35 @@
 <?php
+/**
+ * @author Xanders
+ * @see https://www.linkedin.com/in/xanders-samoth-b2770737/
+ */
 
 namespace App\Http\Controllers\Web;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
 
 class HomeController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['changeLanguage', 'index', 'news', 'newsDatas', 'works', 'workDatas', 'donate', 'about', 'help', 'faq', 'termsOfUse', 'privacyPolicy', 'transactionWaiting', 'transactionMessage', 'registerOffer']);
+    }
+
+    // ==================================== HTTP GET METHODS ====================================
+    /**
+     * GET: Change language
+     *
+     * @param  $locale
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function changeLanguage($locale)
+    {
+        app()->setLocale($locale);
+        session()->put('locale', $locale);
+
+        return Redirect::back();
+    }
+
 }
