a2ce611e3f531c187944c7631ab49f1ac7907146
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-08-24 18:15:43 +0100

    n

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,28 +3,138 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="@lang('miscellaneous.keywords')">
+        <meta name="description" content="">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="jpt-devref" content="IQmxemeH2oYJ7Rsp3yx97S8GEsCVEQdtNaWuh88dfYp66P0HJS8g2xVqEeCnFImCaWKyn733o7jOtzxwB5INSU5W26Bw63QruvZl">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('assets/img/favicon/site.webmanifest') }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,500,500i,600,600i,700,700i&subset=cyrillic" rel="stylesheet">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Font Icons Files -->
+        <link rel="stylesheet" href="{{ asset('assets/icons/bootstrap-icons/bootstrap-icons.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/icons/boxicons/css/boxicons.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/icons/remixicon/remixicon.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/icons/fontawesome/css/all.min.css') }}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css">
+
+        <!-- Addons CSS Files -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/custom/jquery/jquery-ui/jquery-ui.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/custom/dataTables/datatables.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/custom/show-more/dist/css/show-more.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/custom/cropper/css/cropper.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/custom/croppie/croppie.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/startup/owlcarousel/assets/owl.carousel.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/startup/animate/animate.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/custom/bootstrap/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/addons/custom/mdb/css/mdb.min.css') }}">
+
+        <!-- Lonely CSS File -->
+        <script src="{{ asset('assets/css/style.lonely.css') }}"></script>
+        <!-- Custom CSS File -->
+        <script src="{{ asset('assets/css/style.custom.css') }}"></script>
+
+        <title>
+@if (Route::is('home'))
+            J-P Tshienda | @lang('miscellaneous.slogan')
+@endif
+
+@if (Route::is('account') || Route::is('account.update.password'))
+            @lang('miscellaneous.menu.account_settings')
+@endif
+
+@if (Route::is('message.inbox'))
+            @lang('miscellaneous.message.inbox')
+@endif
+
+@if (Route::is('message.outbox'))
+            @lang('miscellaneous.message.outbox')
+@endif
+
+@if (Route::is('message.draft'))
+            @lang('miscellaneous.message.draft')
+@endif
+
+@if (Route::is('message.spams'))
+            @lang('miscellaneous.message.spams')
+@endif
+
+@if (Route::is('message.new'))
+            @lang('miscellaneous.message.new')
+@endif
+
+@if (Route::is('message.search'))
+            @lang('miscellaneous.message.search_result')
+@endif
+
+@if (Route::is('notification'))
+            @lang('miscellaneous.menu.notifications')
+@endif
+
+@if (Route::is('news.home'))
+            @lang('miscellaneous.menu.public.news')
+@endif
+
+@if (Route::is('works'))
+            @lang('miscellaneous.menu.public.works')
+@endif
+
+@if (Route::is('donate'))
+            @lang('miscellaneous.menu.public.donate')
+@endif
+
+@if (Route::is('about.home'))
+            @lang('miscellaneous.public.about.title')
+@endif
+
+@if (Route::is('about.help'))
+            @lang('miscellaneous.public.help.title')
+@endif
+
+@if (Route::is('about.faq'))
+            @lang('miscellaneous.public.faq.title')
+@endif
+
+@if (Route::is('about.terms_of_use'))
+            @lang('miscellaneous.public.terms_of_use.title')
+@endif
+
+@if (Route::is('about.privacy_policy'))
+            @lang('miscellaneous.public.privacy_policy.title')
+@endif
+        </title>
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
+    <body>
+@yield('guest-content')
+
+        <!-- JavaScript Libraries -->
+        <script src="{{ asset('assets/addons/custom/jquery/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/jquery/jquery-ui/jquery-ui.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/mdb/js/mdb.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/dataTables/datatables.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/show-more/dist/js/showMore.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/cropper/js/cropper.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/croppie/croppie.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/autosize/js/autosize.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/startup/wow/wow.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/startup/easing/easing.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/startup/waypoints/waypoints.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/startup/counterup/counterup.min.js') }}"></script>
+        <script src="{{ asset('assets/addons/startup/owlcarousel/owl.carousel.min.js') }}"></script>
+        <script src="{{ asset('assets/js/load-guest-scripts.js') }}"></script>
+        <script src="{{ asset('assets/addons/custom/biliap/js/biliap.cores.js') }}"></script>
+
+        <!-- Startup Javascript -->
+        <script src="{{ asset('assets/js/scripts.startup.js') }}"></script>
+        <!-- Custom Javascript -->
+        <script src="{{ asset('assets/js/scripts.custom.js') }}"></script>
     </body>
 </html>
