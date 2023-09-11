85395880e4c0143762be4b826052de61bdefb356
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-09-11 17:32:49 +0100

    n

---
+++ resources/lang/ln/notifications.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * @author Xanders
+ * @see https://www.linkedin.com/in/xanders-samoth-b2770737/
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    // ===== ERROR PAGES
+    // 400
+    '400_title' => 'Demande ya mabe',
+    '400_description' => 'Verifier demande na yo svp!',
+    // 401
+    '401_title' => 'Oza na ndingisa te',
+    '401_description' => 'Ozali na ndingisa te mpo na bosengi oyo',
+    // 403
+    '403_title' => 'Epekisami',
+    '403_description' => 'Esika oyo epekisami',
+    // 404
+    '404_title' => 'Lokasa emonani te',
+    '404_description' => 'Lokasa ozali koluka eza te',
+    // 405
+    '405_title' => 'Méthode oyo epesami nzela te',
+    '405_description' => 'Lisengi na yo etindami na méthode ya mabe',
+    // 419
+    '419_title' => 'Lokasa esilaki',
+    '419_description' => 'Lokasa yango esalaki ntango molai basaleli te',
+    // 500
+    '500_title' => 'Libunga na kati',
+    '500_description' => 'Serveur na biso ekutanaki na problème moko. Svp bomeka lisusu sima ya mua minutes!',
+    // Others
+    'expects_json' => 'Lisengi ya lelo mbala mosusu ezali kozela eyano ya JSON.',
+
+    // ===== ALERTS
+    'no_record' => 'Enregistrement ezali te!',
+    'create_error' => 'Bokeli elongi te!',
+    'update_error' => 'Mbonguana esalemi te!',
+    'registered_data' => 'Ba données ekoti',
+    'required_fields' => 'Tosɛngi yo otala bisika oyo esengeli',
+    'transaction_waiting' => 'Svp confirmer message oyo ewutaki na opérateur na yo na téléphone na yo. Na nsima, finá butɔ oyo ezali awa na nse.',
+    'transaction_done' => 'Kotinda na yo esili malamu!',
+    'transaction_failed' => 'Elongi te kotinda mbongo na yo.',
+    'transaction_type_error' => 'Svp pona lolenge ya transaction',
+    'new_partner_message' => 'Okoki sikoyo kokota lokola mosangani na nimero na yo ya telefone. Mot de passe ya mwa ntango:',
+    // LegalInfoSubject
+    'find_all_legal_info_subjects_success' => 'Ba sujets emonani',
+    'find_legal_info_subject_success' => 'Sujet emonani',
+    'find_legal_info_subject_404' => 'Sujet emonani te',
+    'create_legal_info_subject_success' => 'Sujet esalemi',
+    'update_legal_info_subject_success' => 'Sujet ebonguani',
+    'delete_legal_info_subject_success' => 'Sujet elongue',
+    // LegalInfoTitle
+    'find_all_legal_info_titles_success' => 'Ba titres emonani',
+    'find_legal_info_title_success' => 'Titre emonani',
+    'find_legal_info_title_404' => 'Titre emonani te',
+    'create_legal_info_title_success' => 'Titre esalemi',
+    'update_legal_info_title_success' => 'Titre ebonguani',
+    'delete_legal_info_title_success' => 'Titre elongue',
+    // LegalInfoContent
+    'find_all_legal_info_contents_success' => 'Ba oyo eza na kati emonani',
+    'find_legal_info_content_success' => 'Oyo eza na kati emonani',
+    'find_legal_info_content_404' => 'Oyo eza na kati emonani te',
+    'create_legal_info_content_success' => 'Oyo eza na kati esalemi',
+    'update_legal_info_content_success' => 'Oyo eza na kati ebonguani',
+    'delete_legal_info_content_success' => 'Oyo eza na kati elongue',
+    // Image
+    'find_all_images_success' => 'Ba images emonani',
+    'find_image_success' => 'Image emonani',
+    'find_image_404' => 'Image emonani te',
+    'create_image_success' => 'Image esalemi',
+    'update_image_success' => 'Image ebonguani',
+    'delete_image_success' => 'Image elongue',
+    // Group
+    'find_all_groups_success' => 'Ba groupes emonani',
+    'find_group_success' => 'Groupe emonani',
+    'find_group_404' => 'Groupe emonani te',
+    'create_group_success' => 'Groupe esalemi',
+    'update_group_success' => 'Groupe ebonguani',
+    'delete_group_success' => 'Groupe elongue',
+    // Status
+    'find_all_statuses_success' => 'Ba états emonani',
+    'find_status_success' => 'Etat emonani',
+    'find_status_404' => 'Etat emonani te',
+    'create_status_success' => 'Etat esalemi',
+    'update_status_success' => 'Etat ebonguani',
+    'delete_status_success' => 'Etat elongue',
+    // Type
+    'find_all_types_success' => 'Ba types emonani',
+    'find_type_success' => 'Type emonani',
+    'find_type_404' => 'Type emonani te',
+    'create_type_success' => 'Type esalemi',
+    'update_type_success' => 'Type ebonguani',
+    'delete_type_success' => 'Type elongue',
+    // Country
+    'find_all_countries_success' => 'Ba mboka emonani',
+    'find_country_success' => 'Mboka emonani',
+    'find_country_404' => 'Mboka emonani te',
+    'create_country_success' => 'Mboka esalemi',
+    'update_country_success' => 'Mboka ebonguani',
+    'delete_country_success' => 'Mboka elongue',
+    // Address
+    'find_all_addresses_success' => 'Ba adresses emonani',
+    'find_address_success' => 'Adresse emonani',
+    'find_address_404' => 'Adresse emonani te',
+    'create_address_success' => 'Adresse esalemi',
+    'update_address_success' => 'Adresse ebonguani',
+    'delete_address_success' => 'Adress elongue',
+    // Role
+    'find_all_roles_success' => 'Ba roles emonani',
+    'find_role_success' => 'Role emonani',
+    'find_role_404' => 'Role emonani te',
+    'create_role_success' => 'Role esalemi',
+    'update_role_success' => 'Role ebonguani',
+    'delete_role_success' => 'Role elongue',
+    // User
+    'find_all_users_success' => 'Basaleli emonani',
+    'find_user_success' => 'Mosaleli amonani',
+    'find_api_token_success' => 'Jeton ya API emonani',
+    'find_user_404' => 'Mosaleli amonani te',
+    'create_user_success' => 'Mosaleli asalemi',
+    'create_user_SMS_failed' => 'Mokakatano moko ezali na service ya SMS',
+    'update_user_success' => 'Mosaleli abonguani',
+    'update_password_success' => 'Password ebonguani',
+    'confirm_password_error' => 'Please confirm your password',
+    'confirm_new_password' => 'Please confirm the new password',
+    'delete_user_success' => 'Mosaleli alongue',
+    // RoleUser
+    'find_all_role_users_success' => 'Ba roles na ba saleli bamonani',
+    'find_role_user_success' => 'Role na mosaleli bamonani',
+    'find_role_user_404' => 'Role na mosaleli bamonani te',
+    'create_role_user_success' => 'Role na mosaleli basalemi',
+    'update_role_user_success' => 'Role na mosaleli babonguani',
+    'delete_role_user_success' => 'Role na mosaleli balongue',
+    // PasswordReset
+    'find_all_password_resets_success' => 'Ba réinitialisations ya mot de passe emonani',
+    'find_password_reset_success' => 'Réinitialisation ya mot de passe emonani',
+    'find_password_reset_404' => 'Réinitialisation ya mot de passe emonani te',
+    'create_password_reset_success' => 'Réinitialisation ya mot de passe esalemi',
+    'update_password_reset_success' => 'Réinitialisation ya mot de passe ebonguani',
+    'delete_password_reset_success' => 'Réinitialisation ya mot de passe elongue',
+    // Message
+    'find_all_messages_success' => 'Ba messages emonani',
+    'find_message_success' => 'Message emonani',
+    'find_message_404' => 'Message emonani te',
+    'create_message_success' => 'Message esalemi',
+    'update_message_success' => 'Message ebonguani',
+    'delete_message_success' => 'Message elongue',
+    // Notification
+    'find_all_notifications_success' => 'Ba notifications emonani',
+    'find_notification_success' => 'Notification emonani',
+    'find_notification_404' => 'Notification emonani te',
+    'create_notification_success' => 'Notification esalemi',
+    'update_notification_success' => 'Notification ebonguani',
+    'delete_notification_success' => 'Notification elongue',
+    // News
+    'find_all_news_success' => 'Bansango emonani',
+    'find_news_success' => 'Sango emonani',
+    'find_news_404' => 'Sango emonani te',
+    'create_news_success' => 'Sango esalemi',
+    'update_news_success' => 'Sango ebonguani',
+    'delete_news_success' => 'Sango elongue',
+    // Offer
+    'find_all_offers_success' => 'Makabo emonani',
+    'find_offer_success' => 'Likabo emonani',
+    'find_offer_404' => 'Likabo emonani te',
+    'create_offer_success' => 'Likabo esalemi',
+    'update_offer_success' => 'Likabo ebonguani',
+    'delete_offer_success' => 'Likabo elongue',
+    // Payment
+    'find_all_payments_success' => 'Mafuti emonani',
+    'find_payment_success' => 'Lifuti emonani',
+    'find_payment_404' => 'Lifuti emonani te',
+    'processing_succeed' => 'Transaction na yo elongi. Okoki komona yango na liste na yo ya mafuti.',
+    'error_while_processing' => 'Libunga moko ekomi ntango ozali kotinda bosenga',
+    'process_failed' => 'Bosenga esimbi te, tosengi omeka lisusu',
+    'process_canceled' => 'O tiki kosala transaction na yo',
+    'create_payment_success' => 'Lifuti esalemi',
+    'update_payment_success' => 'Lifuti ebonguani',
+    'delete_payment_success' => 'Lifuti elongue',
+
+    // ===== PLATFORM NOTIFICATIONS
+    'welcome_user' => 'Boyei malamu na app JPTSHIENDA! Svp tanga ba conditions na biso avant obanda.',
+    'welcome_member' => 'Matondo mingi mpo na kotia motema na Fondation Jean-Pierre TSHIENDA. Tokoyebisa yo noki soki bandimi bozali membre na yo.',
+    'subscribed_to_foundation' => 'akomisi na foudation.',
+    'member_joined' => 'Demande na yo ya membre endimami. Tosɛngi yo otánga mibeko mpe lolene na biso ya kosalela.',
+    'member_locked' => 'Compte na yo ekangami. Soki ozali na mituna, benga biso na nzela ya nimero ya telefone oyo emonisami na site Internet na biso.',
+    'foundation_published' => 'Fondation ebimisi',
+    'foundation_changed' => 'Fondation ebongoli',
+];
